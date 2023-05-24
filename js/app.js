72497e2fd32e863bf7aac9415e401efd5f7c9ec7
Jorge <jorgecapellan98@gmail.com>
2023-05-24 11:39:03 -0400

    add vehicle object

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log('loaded');
+
+class Vehicle {
+    constructor(carName, year, color, ) {
+        this.brand = carName;
+        this.year = year;
+        this.color = color;
+}
+carModel() {
+    console.log('I own a' + this.brand + '.');
+}
+}
+class Model extends Vehicle {
+    constructor(mod) {
+        super(carName);
+        this.model = mod;
+    }
+    show() {
+        return this.carModel() + ', it is a ' + this.model;
+    }
+}
