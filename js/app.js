238e962603e25e6971cdaadf65d3091f5b3c5a3a
Juliza <zepeda.juliza@gmail.com>
2023-05-24 12:38:48 -0300

    added new dog class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log('loaded');
+
+class Dog {
+    constructor(name, age, breed, furColor){
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.furColor = furColor;
+    }
+
+    greet(){
+       console.log(`Hello I'm ${this.name} the dog!`);
+    }
+    hasBirthday(){
+        this.age++;
+    }
+    changeHairColor(newColor) {
+        this.hairColor = newColor;
+    }
+
+}
