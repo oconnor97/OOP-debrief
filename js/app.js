e483db9f82319e10f3b510f784fb6f8adf81ae01
Evan Connerly <connerly.e056764@stu.sanjac.edu>
2023-05-24 10:39:26 -0500

    added new apple script

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('loaded');
+class Apple {
+    constructor(flavor, texture, color, name) {
+        this.flavor =  flavor;
+        this.texture = texture;
+        this.color = color;
+        this.name = name;
+    }
+    description() {
+        console.log('This is a ' + this.name + ' apple. It\'s a ' + this.texture + ', ' + this.color + ', and ' + this.flavor + ' apple.');
+    }
+}
+
+class Jazz extends Apple {
+    constructor(flavor, texture, color, name, seasonality) {
+        super(flavor, texture, color, name);
+        this.seasonality = seasonality
+    }
+    seasonsWhenFreshest(newSeason) {
+        this.seasonality = newSeason;
+    }
+}
