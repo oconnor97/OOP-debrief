d064dfbee8396302eda08af3b65388e6b067df45
Blen <mekuriablen@gmail.com>
2023-05-24 08:40:33 -0700

    add oop class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-console.log('loaded');
+class Pet {
+  constructor(name, type, age) {
+    this.name = name;
+    this.type = type;
+    this.age = age;
+    this.activities();
+    // this.hairColor = hairColor;
+  }
+  greet() {
+    console.log("Hello I am " + this.name + " and I am a " + this.type);
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, type, age) {
+    super(name, type, age);
+    this.funFact = funFact;
+  }
+}
+
+class PetCare extends Cat {
+  constructor(name, type, age) {
+    super(name, type, age);
+    // this.a = [];
+  }
+}
