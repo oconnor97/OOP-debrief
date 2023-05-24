1e71f1bd9cda28fb7666cf19cdb0f2a403cc05fd
eyk261 <eden.y.kifle@gmail.com>
2023-05-24 11:39:46 -0400

    commit add

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log('loaded');
+class School {
+    constructor(name, type, city) {
+        this.name = name;
+        this.type = type;
+        this.city = city;
+    }
+    address() {
+        console.log('The school named' + this.name +' ' + 'with the type of ' + this.type + 'found in the city of ' + this.city);
+    }
+
+}
+class Elementary extends School {
+    constructor(name, type, city, established) {
+        super(name, type, city);
+        this.established = established;
+    }
+    
+}
